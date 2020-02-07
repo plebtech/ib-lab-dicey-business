d7d4a3a17376a2d44416e54d9c6eb2d231adcfc0
Nick Mc <nbmc@protonmail.com>
2020-02-07 15:02:30 -0600

    animated die rolls.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,36 @@
 console.log("Loading..");
 
 function encapsulator() {
-    let counter = 1;
     const container = document.getElementById("container");
     const GENERATE_BTN = document.getElementById("controls").querySelectorAll("button")[0];
+    let counter = 1;
+    let dice = [];
 
     class Die {
         constructor() {
             this.div = document.createElement('div'); // creates div via DOM.
             this.div.className = 'die'; // assigned class to div.
             this.div.id = counter; // assigns id to div (based on counter value).
-            this.value = this.roll(); // holds the roll.
+            this.animate(1000);
             counter++;
-            this.div.innerHTML = '<span>' + setFace(this.value) + '</span>';
+            dice.push(this);
+        }
+        animate(limit) {
             container.appendChild(this.div);
+            let start = Date.now();
+            let timer = setInterval(function () {
+                let timePassed = Date.now() - start;
+                if (timePassed >= limit) {
+                    clearInterval(timer);
+                    return;
+                }
+                this.roll();
+            }.bind(this), 100);
         }
         roll() {
-            return (Math.floor(Math.random() * (7 - 1)) + 1);
+            let r = randomDieVal();
+            this.value = r;
+            this.div.innerHTML = '<span>' + setFace(this.value) + '</span>';
         }
     }
 
@@ -24,21 +38,24 @@ function encapsulator() {
         new Die();
     });
 
+    function randomDieVal() {
+        return Math.floor(Math.random() * (7 - 1)) + 1;
+    }
+
     function setFace(value) {
-        console.log(value);
-        switch(value) {
+        switch (value) {
             case 1:
-                return '\u2680'; 
+                return '\u2680';
             case 2:
-                return '\u2681'; 
+                return '\u2681';
             case 3:
-                return '\u2682'; 
+                return '\u2682';
             case 4:
-                return '\u2683'; 
+                return '\u2683';
             case 5:
-                return '\u2684'; 
+                return '\u2684';
             case 6:
-                return '\u2685'; 
+                return '\u2685';
         }
     }
 }
