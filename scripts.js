292174736efd9bb5164ceb82e0394cb3f71e21dc
Nick Mc <nbmc@protonmail.com>
2020-02-07 15:45:17 -0600

    added reroll and sum functionality.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ console.log("Loading..");
 function encapsulator() {
     const container = document.getElementById("container");
     const GENERATE_BTN = document.getElementById("controls").querySelectorAll("button")[0];
+    const REROLL_BTN = document.getElementById("controls").querySelectorAll("button")[1];
+    const SUM_BTN = document.getElementById("controls").querySelectorAll("button")[2];
     let counter = 1;
     let dice = [];
 
@@ -36,7 +38,36 @@ function encapsulator() {
 
     GENERATE_BTN.addEventListener('click', () => {
         new Die();
+        let noun = setNoun();
+        document.getElementById('status').innerHTML = `<span>Number of ${noun}: ${dice.length}</span>`;
+
     });
+    
+    REROLL_BTN.addEventListener('click', () => {
+        dice.forEach((die) => {
+            die.animate(1000);
+        })
+        let noun = setNoun();
+        document.getElementById('status').innerHTML = `<span>${dice.length} ${noun} rerolled</span>`;
+    });
+
+    SUM_BTN.addEventListener('click', () => {
+        let sum = 0;
+        dice.forEach((die) => {
+            sum+= die.value;
+        });
+        document.getElementById('status').innerHTML = `<span>Sum: ${sum}</span>`;
+    });
+
+    function setNoun() {
+        let n;
+        if (dice.length === 1) {
+            n = 'die';
+        } else if (dice.length > 1) {
+            n = 'dice';
+        }
+        return n;
+    }
 
     function randomDieVal() {
         return Math.floor(Math.random() * (7 - 1)) + 1;
